7821e48324c282ac5f2a5af13bff48c4160c9972
Elliott Pryor <pryorelliott@gmail.com>
2021-07-20 15:41:23 -0600

    Pull changes from master

--- refactoring/FEA/factorevolution.py
+++ refactoring/FEA/factorevolution.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class FEA:
     def __init__(self, function, fea_runs, generations, pop_size, factor_architecture, base_algorithm, continuous=True,
-                 seed=None, callback=None):
+                 seed=None):
         if seed is not None:
             np.random.seed(seed)
 
@@ -20,17 +20,14 @@ class FEA:
         self.set_global_solution(continuous)
         self.subpopulations = self.initialize_factored_subpopulations()
 
-        self.callback = callback  # function called at each fea run, takes 2 param self, fea_run
-
     def run(self):
         for fea_run in range(self.fea_runs):
             for alg in self.subpopulations:
+                # print('NEW SUBPOPULATION\n---------------------------------------------------------------')
                 alg.run()
             self.compete()
             self.share_solution()
             print('fea run ', fea_run, self.global_fitness)
-            if self.callback is not None:
-                self.callback(self, fea_run)
 
     def set_global_solution(self, continuous):
         if continuous:
@@ -42,23 +39,21 @@ class FEA:
         fa = self.factor_architecture
         alg = self.base_algorithm
         return [
-            alg(self.base_alg_iterations, self.pop_size, self.function, len(factor), factor, self.global_solution)
+            alg(function=self.function, dim=len(factor), generations=self.base_alg_iterations, population_size=self.pop_size, factor=factor, global_solution=self.global_solution)
             for factor in fa.factors]
 
     def share_solution(self):
         """
         Construct new global solution based on best shared variables from all swarms
         """
-        gs = self.global_solution
+        gs = [x for x in self.global_solution]
         print('global fitness found: ', self.global_fitness)
         print('===================================================')
         for alg in self.subpopulations:
             # update fitnesses
-            alg.pop = [individual.update_individual_after_compete(individual.position, gs) for individual in alg.pop]
+            alg.pop = [individual.update_individual_after_compete(gs) for individual in alg.pop]
             # set best solution and replace worst solution with global solution across FEA
             alg.replace_worst_solution(gs)
-            curr_best = alg.find_current_best()
-            alg.gbest = min(curr_best, alg.gbest)
 
     def compete(self):
         """
@@ -77,15 +72,17 @@ class FEA:
             for pop_idx in self.factor_architecture.optimizers[var_idx]:
                 curr_pop = self.subpopulations[pop_idx]
                 pop_var_idx = np.where(curr_pop.factor == var_idx)
-                var_candidate_value = curr_pop.gbest.lbest_position[pop_var_idx[0][0]]
+                position = [x for x in curr_pop.gbest.position]
+                var_candidate_value = position[pop_var_idx[0][0]]
                 sol[var_idx] = var_candidate_value
                 new_fitness = f.run(sol)
                 if new_fitness < curr_fitness:
+                    print('smaller fitness found')
                     curr_fitness = new_fitness
                     best_value_for_var = var_candidate_value
             sol[var_idx] = best_value_for_var
         self.global_solution = sol
-        self.global_fitness = curr_fitness
+        self.global_fitness = f.run(sol)
         self.solution_history.append(sol)
 
 
@@ -95,7 +92,7 @@ if __name__ == '__main__':
     from refactoring.FEA.factorarchitecture import FactorArchitecture
 
     fa = FactorArchitecture()
-    fa.load_csv_architecture(file="../../results/factors/F1_overlapping_diff_grouping.csv", dim=50)
+    fa.load_csv_architecture(file="../../results/factors/F1_m4_diff_grouping.csv", dim=50)
     func = Function(function_number=1, shift_data_file="f01_o.txt")
-    fea = FEA(func, 10, 10, 3, fa, PSO)
+    fea = FEA(func, fea_runs=100, generations=1000, pop_size=500, factor_architecture=fa, base_algorithm=PSO)
     fea.run()
