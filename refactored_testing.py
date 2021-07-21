89e2da0e83aaebcd166f33d5de5ec0ee89f9bc65
Elliott Pryor <pryorelliott@gmail.com>
2021-07-21 16:26:28 -0600

    Setup for big tests
    Set to run full experiments
    Parameters
    FEA_ITERATIONS: 25
    POP_SIZE: 1000
    PSO_ITERATIONS: 120
    
    PSO_ITERATIONS was set to be 3e6/(FEA * POP) based on CEC2010 paper for
    number of func evals=3e6.

--- refactored_testing.py
+++ refactored_testing.py
@@ -16,13 +16,13 @@ def output_callback(output_file, fea, fea_run):
 
 
 if __name__ == '__main__':
-    dim = 200
+    dim = 1000
 
     if len(sys.argv) == 4:
-        outputcsv = open(f'./MeetRandom/tuning_{dim}_f{int(sys.argv[1])}.csv', 'a')
+        outputcsv = open(f'./MeetRandom/Trial_{dim}_f{int(sys.argv[1])}.csv', 'a')
         f = Function(int(sys.argv[1]), shift_data_file=sys.argv[2], matrix_data_file=sys.argv[3])
     elif len(sys.argv) == 3:
-        outputcsv = open(f'./MeetRandom/tuning_{dim}_f{int(sys.argv[1])}.csv', 'a')
+        outputcsv = open(f'./MeetRandom/Trial_{dim}_f{int(sys.argv[1])}.csv', 'a')
         f = Function(int(sys.argv[1]), shift_data_file=sys.argv[2])
     else:
         outputcsv = open('MeetRandom/tuning_200_f5.csv', 'a')
@@ -31,53 +31,120 @@ if __name__ == '__main__':
 
 
     # outputfile.write("Dim: " + str(dim) + " Seed = 1 \t ODG and DG\n")
-    k = "Pop, PSO,FEA_ITER, ODG"
+    dim = 1000
+    # outputfile.write("Dim: " + str(dim) + " Seed = 1 \t ODG and DG\n")
+    random_iteration = [5, 45, 50, 100, 300, 500, 1000, 3000, 5000, 10000]  # 5, 50, 100, 200, 500, 1000, 2000, 5000, 10000
+    k = "PSO,DG,ODG,MEET," + ','.join([f'Rand {sum(random_iteration[0:i + 1])}' for i in range(len(random_iteration))])
     outputcsv.write(k + '\n')
 
     odg = FactorArchitecture(dim=dim)
     print('starting odg')
     odg.overlapping_diff_grouping(f, 0.001)
-    odg.save_architecture('MeetRandom/architectures/odg')
+    odg.save_architecture('MeetRandom/architectures/odg.arch')
 
     dg = FactorArchitecture(dim=dim)
     print('starting dg')
     dg.diff_grouping(f, 0.001)
-    dg.save_architecture('MeetRandom/architectures/dg')
+    dg.save_architecture('MeetRandom/architectures/dg.arch')
 
+    print("Starting MEET IM")
+    im = MEE(f, dim, 3000, 0, 0.001, 0.000001, use_mic_value=True)
+    IM = im.get_IM()
+    print("finished IM")
+    meet = FactorArchitecture(dim=dim)
+    meet.MEET(IM)
+    print("finished MEET")
+    meet.save_architecture("MeetRandom/meet.arch")
 
-    callback_func = functools.partial(output_callback, outputcsv)
-    pso_options = [10, 15, 20]
-    pop_size = [50, 100, 300, 500]
-    fea_max = 25
-    for i in range(3):
-        for pop in pop_size:
-            for pso_iter in pso_options:
-                print(f"I: {i}\tpop: {pop}\tPSO_ITER: {pso_iter}")
-                outputcsv.write(f"{pop}, {pso_iter}, , \n")
+    fea_iter = 25
+    pop_size = 1000
+    func_evals = 3000000
+    for i in range(25):
+        outputfile = open('./MeetRandom/trial.txt', 'a')
+        print("running")
 
-                outputfile = open('./MeetRandom/tuning.txt', 'a')
-                print("running")
+        summary = {}
+        outputfile.write("Dim: " + str(dim) + " Random Init\n")
 
-                summary = {}
-                outputfile.write("Dim: " + str(dim) + " Random Init\n")
+        total = 0
+        im = RandomTree(f, dim, 3000, 0.001, 0.000001)
+        for it in random_iteration:
+            total += it
 
-                fa = FactorArchitecture()
-                print("FEA ODG")
-                fa.load_architecture("MeetRandom/architectures/odg")
-                fea = FEA(f, fea_max, pso_iter, pop, fa, PSO, seed=i, callback=callback_func)
-                fea.run()
-                outputfile.write(f"ODG, \t\t{fea.global_fitness}\n")
-                print(fea.global_fitness)
-                summary['ODG'] = fea.global_fitness
+            print("Starting Random " + str(total))
+            T = im.run(it)
+            print("finished Random " + str(total))
+            meet = FactorArchitecture(dim=dim)
+            meet.MEET(T)
+            print("finished Random " + str(total))
+            meet.save_architecture("MeetRandom/rand" + str(total))
 
-                print(summary)
+            # factor_graphing(meet.factors, f"./MeetRandom/imgs/rand{total}/")
 
-                # keys = k.split(',')
-                # if all(elem in keys for elem in summary.keys()):
-                #     print("writing to file")
-                #     line_out = ','.join([str(summary[key]) for key in keys])
-                #     print(line_out)
-                #     outputcsv.write(line_out + '\n')
-                # else:
-                #     print(f'{summary.keys()} != {keys}')
-                outputfile.close()
+        # Skip MEET for time
+        # print("Starting MEET IM")
+        # im = MEE(f, dim, 3000, 0, 0.001, 0.000001, use_mic_value=True)
+        # IM = im.get_IM()
+        # print("finished IM")
+        # meet = FactorArchitecture(dim=dim)
+        # meet.MEET(IM)
+        # print("finished MEET")
+        # meet.save_architecture("MeetRandom/meet")
+
+        # factor_graphing(meet.factors, "./MeetRandom/imgs/meet/")
+
+        fa = FactorArchitecture()
+        print("FEA MEET")
+        fa.load_architecture("MeetRandom/meet.arch")
+        fea = FEA(f, fea_iter, int(func_evals / (fea_iter * pop_size)), pop_size, fa, PSO, seed=i)
+        fea.run()
+        outputfile.write(f"MEET, \t\t{fea.global_fitness}\n")
+        print(fea.global_fitness)
+        summary['MEET'] = fea.global_fitness
+
+        total = 0
+        for it in random_iteration:
+            total += it
+            fa = FactorArchitecture()
+            print("FEA Rand " + str(total))
+            fa.load_architecture("MeetRandom/rand" + str(total))
+            fea = FEA(f, fea_iter, int(func_evals / (fea_iter * pop_size)), pop_size, fa, PSO, seed=i)  # pso iter = 120
+            fea.run()
+            outputfile.write(f"Rand {total}, \t{fea.global_fitness}\n")
+            print(fea.global_fitness)
+
+            summary[f'Rand {total}'] = fea.global_fitness
+
+        fa = FactorArchitecture()
+        print("FEA ODG")
+        fa.load_architecture("MeetRandom/odg.arch")
+        fea = FEA(f, fea_iter, int(func_evals / (fea_iter * pop_size)), pop_size, fa, PSO, seed=i)
+        fea.run()
+        outputfile.write(f"ODG, \t\t{fea.global_fitness}\n")
+        print(fea.global_fitness)
+        summary['ODG'] = fea.global_fitness
+
+        fa = FactorArchitecture()
+        print("FEA DG")
+        fa.load_architecture("MeetRandom/dg.arch")
+        fea = FEA(f, fea_iter, int(func_evals / (fea_iter * pop_size)), pop_size, fa, PSO, seed=i)
+        fea.run()
+        outputfile.write(f"DG, \t\t{fea.global_fitness}\n")
+        print(fea.global_fitness)
+        summary['DG'] = fea.global_fitness
+
+        print("PSO")
+        pso = PSO(generations=int(func_evals / pop_size), population_size=pop_size, function=f, dim=dim)  # generations=3000
+        gbest = pso.run()
+        summary['PSO'] = pso.gbest.fitness
+
+        print(summary)
+
+        keys = k.split(',')
+        if all(elem in keys for elem in summary.keys()):
+            line_out = ','.join([str(summary[key]) for key in keys])
+            outputcsv.write(line_out + '\n')
+            outputcsv.flush()
+        else:
+            print(f'{summary.keys()} != {keys}')
+        outputfile.close()
