d2ce1e164e52f1b0dd165da1d358c9e61d8692bd
ElliottP-13 <pryorelliott@gmail.com>
2021-08-19 23:50:16 +0000

    Everything from kraken

--- dg_testing.py
+++ dg_testing.py
@@ -15,19 +15,25 @@ if __name__ == '__main__':
     f5 = Function(5, shift_data_file='f05_op.txt', matrix_data_file='f05_m.txt')
     f11 = Function(11, shift_data_file='f11_op.txt', matrix_data_file='f11_m.txt')
     f17 = Function(17, shift_data_file='f17_op.txt')
-    f20 = Function(19, shift_data_file='f20_o.txt')
+    f20 = Function(20, shift_data_file='f20_o.txt')
 
-    f = f20
-    method = 'odg'
+    if len(sys.argv) > 1:
+        function_dic = {'3': f3, '5': f5, '11': f11, '17': f17, '20': f20}
+        f = function_dic[sys.argv[1]]
+#        method = sys.argv[2]
+    else:
+        f = f20
+#        method = 'odg'
 
-    fa = FactorArchitecture()
-    fa.load_architecture(f"MeetRandom/{f.function_to_call}_{method}")
-
-    print(len(fa.factors))
-    total = 0
-    for fac in fa.factors:
-        total += len(fac)
-    print(total / len(fa.factors))
-    print(fac)
-
-
+    methods = ['odg', 'dg', 'rand', '2_rand']
+    
+    for method in methods:
+        fa = FactorArchitecture()
+        fa.load_architecture(f"MeetRandom/{f.function_to_call}_{method}")
+    
+        print(len(fa.factors))
+        total = 0
+        for fac in fa.factors:
+            total += len(fac)
+        print(total / len(fa.factors))
+        print(fa.factors)
