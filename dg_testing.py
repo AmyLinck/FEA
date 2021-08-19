9a87c3581ab2dcc1d335d8c5e97a78af2ff2553a
ElliottP-13 <pryorelliott@gmail.com>
2021-08-19 14:43:41 -0400

    factor testing

--- dg_testing.py
+++ dg_testing.py
@@ -17,34 +17,17 @@ if __name__ == '__main__':
     f17 = Function(17, shift_data_file='f17_op.txt')
     f20 = Function(19, shift_data_file='f20_o.txt')
 
-    functions = [f3, f5, f11, f17, f20]
-    files = ['f03.txt', 'f05.txt', 'f11.txt', 'f17.txt', 'f20.txt']
+    f = f20
+    method = 'odg'
 
-    dg_epsilon = 0.001
-    dim = 1000
+    fa = FactorArchitecture()
+    fa.load_architecture(f"MeetRandom/{f.function_to_call}_{method}")
 
-    for i, f in enumerate(functions):
-        # f.evals = 0
-        # odg = FactorArchitecture(dim=dim)
-        # odg.overlapping_diff_grouping(f, dg_epsilon)
-        # odg.save_architecture(f'MeetRandom/{f.function_to_call}_odg')
-
-        dg = FactorArchitecture(dim=dim)
-        print('starting dg')
-        f.evals = 0
-        dg.diff_grouping(f, dg_epsilon)
-        dg.save_architecture(f'MeetRandom/{f.function_to_call}_dg')
-        dg.get_factor_topology_elements()
-
-        f.evals = 0
-        fea = FEA(f, 50, 15, 10, dg, PSO, seed=1)
-        fea_run, pso_runs = fea.run()
-        print(f"DG, \t\t{len(fa.factors)}, {fea.global_fitness}\n")
-
-        continue
-
-        fa = FactorArchitecture(dim = 1000)
-        fa.load_txt_architecture(files[i])
-        print(len(fa.factors))
+    print(len(fa.factors))
+    total = 0
+    for fac in fa.factors:
+        total += len(fac)
+    print(total / len(fa.factors))
+    print(fac)
 
 
